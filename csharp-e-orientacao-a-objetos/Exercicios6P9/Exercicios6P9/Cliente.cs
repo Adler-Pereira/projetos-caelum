66041bf5ea69f0d624a67764473a47f564d3883d
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-05-05 10:17:47 -0300

    (Exercicios6P9): finalização do exercício opcional

--- csharp-e-orientacao-a-objetos/Exercicios6P9/Exercicios6P9/Cliente.cs
+++ csharp-e-orientacao-a-objetos/Exercicios6P9/Exercicios6P9/Cliente.cs
@@ -9,8 +9,15 @@ namespace Exercicios6P9
     internal class Cliente
     {
         public string nome;
+        public int idade;
         public string rg;
         public string cpf;
         public string endereco;
+
+        public bool EhMaiorDeIdade(int idade)
+        {
+            if (idade >= 18) return true;
+            return false;
+        }
     }
 }
