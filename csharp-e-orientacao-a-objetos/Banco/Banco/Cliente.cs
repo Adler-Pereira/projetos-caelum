f80b995869cecd84a10a68090d4d4ea67c5956ea
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-05-07 13:58:25 -0300

    Criação do projeto Banco com as classes Conta e Cliente

---
+++ csharp-e-orientacao-a-objetos/Banco/Banco/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Banco
+{
+    public class Cliente
+    {
+        public Cliente(string v)
+        {
+            Nome = v;
+        }
+
+        public string Nome { get; set; }
+    }
+}
