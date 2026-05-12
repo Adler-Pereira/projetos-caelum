df190a589520f162008805c97a32f58375e2630f
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-05-12 14:20:30 -0300

    (Exercicios11P2): finalização dos exercícios necessários

--- csharp-e-orientacao-a-objetos/Exercicio9P8/Exercicio9P8/Form1.Designer.cs
+++ csharp-e-orientacao-a-objetos/Exercicio9P8/Exercicio9P8/Form1.Designer.cs
@@ -39,20 +39,27 @@
             this.botaoDepositar = new System.Windows.Forms.Button();
             this.botaoSacar = new System.Windows.Forms.Button();
             this.btTotalConta = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btBusca = new System.Windows.Forms.Button();
+            this.textoIndice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textoTitular
             // 
-            this.textoTitular.Location = new System.Drawing.Point(55, 71);
+            this.textoTitular.Location = new System.Drawing.Point(112, 173);
             this.textoTitular.Name = "textoTitular";
-            this.textoTitular.Size = new System.Drawing.Size(184, 20);
+            this.textoTitular.Size = new System.Drawing.Size(228, 20);
             this.textoTitular.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(55, 52);
+            this.label1.Location = new System.Drawing.Point(33, 176);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 1;
@@ -62,7 +69,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(55, 109);
+            this.label2.Location = new System.Drawing.Point(33, 202);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 3;
@@ -70,23 +77,23 @@
             // 
             // textoSaldo
             // 
-            this.textoSaldo.Location = new System.Drawing.Point(55, 128);
+            this.textoSaldo.Location = new System.Drawing.Point(112, 199);
             this.textoSaldo.Name = "textoSaldo";
-            this.textoSaldo.Size = new System.Drawing.Size(184, 20);
+            this.textoSaldo.Size = new System.Drawing.Size(228, 20);
             this.textoSaldo.TabIndex = 2;
             // 
             // textoNumero
             // 
-            this.textoNumero.Location = new System.Drawing.Point(55, 185);
+            this.textoNumero.Location = new System.Drawing.Point(112, 225);
             this.textoNumero.Name = "textoNumero";
-            this.textoNumero.Size = new System.Drawing.Size(184, 20);
+            this.textoNumero.Size = new System.Drawing.Size(228, 20);
             this.textoNumero.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(55, 166);
+            this.label3.Location = new System.Drawing.Point(33, 228);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 3;
@@ -94,16 +101,16 @@
             // 
             // textoValor
             // 
-            this.textoValor.Location = new System.Drawing.Point(55, 243);
+            this.textoValor.Location = new System.Drawing.Point(112, 251);
             this.textoValor.Name = "textoValor";
-            this.textoValor.Size = new System.Drawing.Size(184, 20);
+            this.textoValor.Size = new System.Drawing.Size(228, 20);
             this.textoValor.TabIndex = 2;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(55, 224);
+            this.label4.Location = new System.Drawing.Point(33, 254);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(31, 13);
             this.label4.TabIndex = 3;
@@ -111,7 +118,8 @@
             // 
             // botaoDepositar
             // 
-            this.botaoDepositar.Location = new System.Drawing.Point(55, 306);
+            this.botaoDepositar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.botaoDepositar.Location = new System.Drawing.Point(170, 164);
             this.botaoDepositar.Name = "botaoDepositar";
             this.botaoDepositar.Size = new System.Drawing.Size(75, 23);
             this.botaoDepositar.TabIndex = 4;
@@ -121,7 +129,8 @@
             // 
             // botaoSacar
             // 
-            this.botaoSacar.Location = new System.Drawing.Point(167, 306);
+            this.botaoSacar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.botaoSacar.Location = new System.Drawing.Point(251, 164);
             this.botaoSacar.Name = "botaoSacar";
             this.botaoSacar.Size = new System.Drawing.Size(75, 23);
             this.botaoSacar.TabIndex = 5;
@@ -131,23 +140,75 @@
             // 
             // btTotalConta
             // 
-            this.btTotalConta.Location = new System.Drawing.Point(55, 345);
+            this.btTotalConta.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btTotalConta.Location = new System.Drawing.Point(22, 164);
             this.btTotalConta.Name = "btTotalConta";
-            this.btTotalConta.Size = new System.Drawing.Size(187, 23);
+            this.btTotalConta.Size = new System.Drawing.Size(103, 23);
             this.btTotalConta.TabIndex = 6;
             this.btTotalConta.Text = "Totalizar Contas";
             this.btTotalConta.UseVisualStyleBackColor = true;
             this.btTotalConta.Click += new System.EventHandler(this.btTotalConta_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btTotalConta);
+            this.groupBox1.Controls.Add(this.botaoDepositar);
+            this.groupBox1.Controls.Add(this.botaoSacar);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(14, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 215);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conta";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btBusca);
+            this.groupBox2.Controls.Add(this.textoIndice);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox2.Location = new System.Drawing.Point(15, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(352, 85);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Busca de Conta";
+            // 
+            // btBusca
+            // 
+            this.btBusca.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btBusca.Location = new System.Drawing.Point(250, 37);
+            this.btBusca.Name = "btBusca";
+            this.btBusca.Size = new System.Drawing.Size(75, 23);
+            this.btBusca.TabIndex = 2;
+            this.btBusca.Text = "Buscar";
+            this.btBusca.UseVisualStyleBackColor = true;
+            this.btBusca.Click += new System.EventHandler(this.btBusca_Click);
+            // 
+            // textoIndice
+            // 
+            this.textoIndice.Location = new System.Drawing.Point(108, 39);
+            this.textoIndice.Name = "textoIndice";
+            this.textoIndice.Size = new System.Drawing.Size(136, 20);
+            this.textoIndice.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Escolha a conta";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btTotalConta);
-            this.Controls.Add(this.botaoSacar);
-            this.Controls.Add(this.botaoDepositar);
+            this.ClientSize = new System.Drawing.Size(382, 366);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -156,9 +217,13 @@
             this.Controls.Add(this.textoSaldo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textoTitular);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,6 +242,11 @@
         private System.Windows.Forms.Button botaoDepositar;
         private System.Windows.Forms.Button botaoSacar;
         private System.Windows.Forms.Button btTotalConta;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btBusca;
+        private System.Windows.Forms.TextBox textoIndice;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- csharp-e-orientacao-a-objetos/Exercicio9P8/Exercicio9P8/Form1.cs
+++ csharp-e-orientacao-a-objetos/Exercicio9P8/Exercicio9P8/Form1.cs
@@ -12,7 +12,8 @@ namespace Exercicio9P8
 {
     public partial class Form1 : Form
     {
-        private Conta c;
+        private Conta[] contas;
+        private Conta selecionada;
         private TotalizadorDeContas totalContas = new TotalizadorDeContas();
         public Form1()
         {
@@ -21,31 +22,47 @@ namespace Exercicio9P8
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            c = new Conta();
-            c.Titular = new Titular("Victor");
-            c.Numero = 1;
-            textoTitular.Text = c.Titular.Nome;
-            textoSaldo.Text = Convert.ToString(c.Saldo);
-            textoNumero.Text = Convert.ToString(c.Numero);
+            contas = new Conta[3];
+            this.contas[0] = new Conta();
+            this.contas[0].Titular = new Titular("Victor");
+            this.contas[0].Numero = 1;
+
+            this.contas[1] = new ContaPoupanca();
+            this.contas[1].Titular = new Titular("Mauricio");
+            this.contas[1].Numero = 2;
+
+            this.contas[2] = new ContaCorrente();
+            this.contas[2].Titular = new Titular("Osni");
+            this.contas[2].Numero = 3;
         }
 
         private void botaoDepositar_Click(object sender, EventArgs e)
         {
             double valorDeposito = Convert.ToDouble(textoValor.Text);
-            c.Deposita(valorDeposito);
-            textoSaldo.Text = Convert.ToString(c.Saldo);
+            selecionada.Deposita(valorDeposito);
+            textoSaldo.Text = Convert.ToString(selecionada.Saldo);
         }
 
         private void botaoSacar_Click(object sender, EventArgs e)
         {
             double valorDeposito = Convert.ToDouble(textoValor.Text);
-            c.Saca(valorDeposito);
-            textoSaldo.Text = Convert.ToString(c.Saldo);
+            selecionada.Saca(valorDeposito);
+            textoSaldo.Text = Convert.ToString(selecionada.Saldo);
         }
 
         private void btTotalConta_Click(object sender, EventArgs e)
         {
-            totalContas.Adiciona(c);
+            totalContas.Adiciona(selecionada);
+        }
+
+        private void btBusca_Click(object sender, EventArgs e)
+        {
+            int indice = Convert.ToInt32(textoIndice.Text);
+            selecionada = this.contas[indice];
+
+            textoTitular.Text = selecionada.Titular.Nome;
+            textoSaldo.Text = Convert.ToString(selecionada.Saldo);
+            textoNumero.Text = Convert.ToString(selecionada.Numero);
         }
     }
 }
