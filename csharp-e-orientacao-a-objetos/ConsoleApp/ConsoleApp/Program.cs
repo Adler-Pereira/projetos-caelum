b23852fd3147e9098204513afa0f461ee3fa1ba4
adler.lima AL. <adler.lima@tecdetbrg.com.br>
2026-05-27 10:36:58 -0300

    (Exercicios22P4): finalização dos exercícios opcionais

---
+++ csharp-e-orientacao-a-objetos/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Mensagem que vai para o terminal");
+
+using (TextReader leitor = Console.In) {
+
+    string linha = leitor.ReadLine();
+
+    while (linha != null)
+    {
+        Console.WriteLine(linha);
+        linha = leitor.ReadLine();
+    }
+}
